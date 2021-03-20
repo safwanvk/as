f451b831962b7a13a377575e8d0d0a37f6851d7d
safwanvk <safwanvalakundil@gmail.com>
2021-03-20 12:42:38 +0530

    :sparkles: Setup table student

--- student/models.py
+++ student/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    sid = models.IntegerField(null=False, primary_key=True)
+    name = models.CharField(max_length=64)
+    username = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "student"
