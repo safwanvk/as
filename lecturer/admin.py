1e0db08a4f333358709617cf342102fc98e58479
safwanvk <safwanvalakundil@gmail.com>
2021-03-20 13:46:05 +0530

    :sparkles: Setup table StudentLoginSessions

--- lecturer/admin.py
+++ lecturer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Lecturer
+from .models import Lecturer, LecturerLoginSession
 
 # Register your models here.
 admin.site.register(Lecturer)
+admin.site.register(LecturerLoginSession)
--- student/models.py
+++ student/models.py
@@ -11,3 +11,15 @@ class Student(models.Model):
 
     class Meta:
         db_table = "Student"
+
+class StudentLoginSessions(models.Model):
+    sid = models.ForeignKey(Student,to_field='sid',on_delete=models.CASCADE, null=False)
+    session_id = models.CharField(max_length=32,null=False)
+    expires = models.DateTimeField(null=False)
+
+
+    def __str__(self):
+        return self.sid
+
+    class Meta:
+        db_table = "StudentLoginSessions"
