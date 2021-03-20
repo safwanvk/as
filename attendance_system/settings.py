17a331e0c7126b03759e2fdc41c0f6ef776acdff
safwanvk <safwanvalakundil@gmail.com>
2021-03-20 12:12:36 +0530

    :sparkles: Setup app config

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'student',
+    'lecturer',
+    'event',
+    'attendance'
+
 ]
 
 MIDDLEWARE = [
