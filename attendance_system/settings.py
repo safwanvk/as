eeb5802e392fe570940efa4e7dc9d4659ae237b1
safwanvk <safwanvalakundil@gmail.com>
2021-03-20 12:25:07 +0530

    :sparkles: Setup db with mysql

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'attendance_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'as', # example - blog_data
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
