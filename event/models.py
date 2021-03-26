c507b564044e8ace71e0b8c69f5324e489297647
safwanvk <safwanvalakundil@gmail.com>
2021-03-26 16:41:46 +0530

    :sparkles: Setup Building table

--- event/models.py
+++ event/models.py
@@ -14,3 +14,14 @@ class Event(models.Model):
 
     class Meta:
         db_table = "Event"
+
+class Building(models.Model):
+    code = models.CharField(max_length=2,null=False, primary_key=True)
+    building = models.CharField(max_length=64)
+    coordinates = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        db_table = "Building"
